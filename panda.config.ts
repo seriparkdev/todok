043b1660f3ac6ac526898d411195693f818fa64f
seriparkdev <parkseridev@gmail.com>
2025-10-23 19:23:29 +0900

    chore: 색상 토큰 세팅

--- panda.config.ts
+++ panda.config.ts
@@ -1,6 +1,15 @@
+import { colors } from './src/constants/style/colors';
 import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   preflight: true,
-  include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
+  include: ['./src/components/**/*.{ts,tsx,js,jsx}', './app/**/*.{ts,tsx,js,jsx}'],
+
+  theme: {
+    extend: {
+      tokens: {
+        colors,
+      },
+    },
+  },
 });
---
+++ src/constants/style/colors.ts
@@ -0,0 +1,30 @@
+export const colors = {
+  gray: {
+    50: { value: '#f9fafb' },
+    100: { value: '#f3f4f6' },
+    200: { value: '#e5e7eb' },
+    300: { value: '#d1d5db' },
+    400: { value: '#9ca3af' },
+    500: { value: '#6b7280' },
+    600: { value: '#4b5563' },
+    700: { value: '#374151' },
+    800: { value: '#1f2937' },
+    900: { value: '#111827' },
+    950: { value: '#030712' },
+  },
+  success: {
+    50: { value: '#f0fdf4' },
+    500: { value: '#22c55e' },
+    600: { value: '#16a34a' },
+  },
+  warning: {
+    50: { value: '#fffbeb' },
+    500: { value: '#f59e0b' },
+    600: { value: '#d97706' },
+  },
+  error: {
+    50: { value: '#fef2f2' },
+    500: { value: '#ef4444' },
+    600: { value: '#dc2626' },
+  },
+} as const;
