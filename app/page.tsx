354954aa02fb865cc23050754a2de1c781825458
seriparkdev <parkseridev@gmail.com>
2025-10-23 19:23:14 +0900

    chore: 파일 구조 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { css } from '../../styled-system/css';
+import { css } from '../styled-system/css';
 
 export default function Home() {
   return <div className={css({ color: 'red' })}>hello world</div>;
