3dc8f37547dd83eb374afba4e5f74f28e5629366
seriparkdev <parkseridev@gmail.com>
2025-12-22 16:03:32 +0900

    fix:  호환성 문제로 storybook webpack 이전

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,22 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import path from 'path';
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs'],
   framework: {
-    name: '@storybook/nextjs-vite',
+    name: '@storybook/nextjs',
     options: {},
+  },
+  webpackFinal: async config => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(process.cwd(), 'src'),
+        '@styled': path.resolve(process.cwd(), 'styled-system'),
+      };
+    }
+    return config;
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from '@storybook/nextjs-vite';
+import type { Preview } from '@storybook/nextjs';
 import '../app/globals.css';
 
 const preview: Preview = {
